315362995e595574be8e1974154b2be8f0162d29
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2022-02-25 00:05:32 -0600

    added footer and menu to the app, simply by adding their selectors in the app.component.html and added bootstrap decoration with routerLink routng

--- project/frontend/todo/src/app/app.module.ts
+++ project/frontend/todo/src/app/app.module.ts
@@ -8,9 +8,22 @@ import { TavoComponent } from './tavo/tavo.component';
 import { LoginComponent } from './login/login.component';
 import { ErrorsComponent } from './errors/errors.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
-
+import { MenuComponent } from './menu/menu.component';
+import { FooterComponent } from './footer/footer.component';
+//NgModule is the angular module
+//What is this? How is it different from JS modules?
+//Notes in the google docs notes
 @NgModule({
-  declarations: [AppComponent, WelcomeComponent, TavoComponent, LoginComponent, ErrorsComponent, ListTodosComponent],
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    TavoComponent,
+    LoginComponent,
+    ErrorsComponent,
+    ListTodosComponent,
+    MenuComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   //Added FormsModule so that we can use the [(ngModel)]="username"
   providers: [],
--- project/frontend/todo/src/main.ts
+++ project/frontend/todo/src/main.ts
@@ -3,10 +3,14 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+//Loading AppModule and bootstraping it.
+//Whichever module which is firstly bootstrapped in the angular application is called root module
+//AppModule gets initialized -> AppComponent gets initialized -> we get app.component.html -> we use that in index.html
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
