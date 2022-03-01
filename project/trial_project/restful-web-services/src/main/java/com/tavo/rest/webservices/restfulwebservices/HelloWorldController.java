413f5f56d04b301a35ea1c6d0c58a942bac12437
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2022-02-28 21:29:56 -0600

    Added a springboot initialized application, which is running in 8081 port, and is doing nothing for now. SDK used:17. The view in the folder is exactly as it was downloaded (commented before I proceeded to add the RESTful service code)

---
+++ project/trial_project/restful-web-services/src/main/java/com/tavo/rest/webservices/restfulwebservices/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.tavo.rest.webservices.restfulwebservices;
+
+//We want to tell spring that this is a controller
+//@Rest
+public class HelloWorldController {
+	//we want to create a method that returns "Hello World"
+	//whenever we define a Web service, we want to define:
+	/*
+	 * 1) What Method: GET
+	 * 2) What URI: /hello-world
+	 * So this program works as follows:
+	 * if someone goes to the browser to the /hello-world
+	 * and sends a get requests there, all I want to send is
+	 * some text.
+	 */
+	//public String helloWorld() {
+	//	return "Hello World";
+	//}
+}
