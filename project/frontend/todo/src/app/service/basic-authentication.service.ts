eb1c2ce6f0eea43c09362544c5c48d10997a3bb0
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2022-03-18 23:51:33 -0600

    no functional changes, just cleaning the code

--- project/frontend/todo/src/app/service/basic-authentication.service.ts
+++ project/frontend/todo/src/app/service/basic-authentication.service.ts
@@ -11,20 +11,6 @@ export class BasicAuthenticationService {
   //we can innject this service in any place we want
   constructor(private http: HttpClient) {}
 
-  // authenticate(username: string, password: string) {
-  //   //console.log('before ' + this.isUserLoggedIn());
-  //   if (username == 'tavin' && password == 'dummy') {
-  //     sessionStorage.setItem('authenticatedUser', username);
-  //     //We can see this by accessing the website's inspect, then Application
-  //     //We see a dictionary of sessionStorage, which has as key authenticatedUser
-  //     //and as value the username logged in
-  //     //console.log('after ' + this.isUserLoggedIn());
-
-  //     return true;
-  //   }
-  //   return false;
-  // }
-
   /**
    * We don't need that code anymore
    *
